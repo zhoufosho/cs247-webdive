a54255a5d60c3024ed6fff6857459e94d6701915
Elaine Zhou <ezhou@stanford.edu>
2015-10-06 23:10:30 -0700

    fix some rendering issues

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,32 @@
 $(function() {
 	prettyPrint();
+	showActiveLink();
 });
 
 function prettyPrint() {
 	$('.snippet').each(function(i) {
 		var snippet = $(this);
 		var pp = snippet.find(".prettyprint").first();
-		if (pp.hasClass('no-esp') || !snippet.find('.explanation').first()) {
+		if (pp.hasClass('no-esp') || !snippet.find('.rendered-result').first()) {
 			return;
 		}
 
-		snippet.find('.explanation').first().html(pp.html());
+		snippet.find('.rendered-result').first().html(pp.html());
 		pp.text(pp.html());
 	});
+}
+
+function showActiveLink() {
+	$('.nav-links li').removeClass('active');
+	var linkName = window.location.href.split("/").pop();
+	var navLinks =  $('a[href$="' + linkName + '"]');
+	navLinks.each(function(i) {
+		var navLink = $(this);
+		navLink.closest("li").addClass("active"); //Set click li as active
+	});
+
+	if (navLinks.length === 0) {
+		$('.home-nav').closest("li").addClass("active");
+	}
+
 }
