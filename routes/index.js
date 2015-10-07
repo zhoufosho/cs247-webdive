88148a659d05834be7d2eae0326c233f9bee1039
Elaine Zhou <ezhou@stanford.edu>
2015-10-06 17:51:58 -0700

    add css stuff

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CS247 Web Bootcamp' });
+  res.render('index', { title: 'CS247 Web Bootcamp'});
+});
+
+router.get('/html', function(req, res, next) {
+  res.render('html', { title: 'HTML Refresher' });
+});
+
+router.get('/css', function(req, res, next) {
+  res.render('css', { title: 'CSS Cliffnotes' });
+});
+
+router.get('/js', function(req, res, next) {
+  res.render('js', { title: 'Javascript Jam' });
 });
 
 module.exports = router;
