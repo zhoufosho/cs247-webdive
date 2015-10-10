c8f8aff621e530b6fe126629fd31cb60a87beea4
Elaine Zhou <ezhou@stanford.edu>
2015-10-09 21:19:32 -0700

    nicer splash, add analytics, change link style

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CS247 Web Bootcamp'});
+  res.render('index', { title: 'CS247 Web Bootcamp', homepage: true});
 });
 
 router.get('/html', function(req, res, next) {
